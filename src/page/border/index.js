cc582063a7de0fa233d4196ad742da9c86ffb0fe
julivansoares <julivan.soares@outlook.com>
2023-01-23 21:32:39 -0300

    finalizado a responsividade

--- src/page/border/index.js
+++ src/page/border/index.js
@@ -1,7 +1,18 @@
 
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './style.css';
 
 function BorderRadius() {
+
+  const [border, setBorde] = useState({ topL: 0, topR: 0, bottomL: 0, bottomR: 0 })
+
+  useEffect(() => {
+    const div = document.querySelector('.sub-container');
+    div.style.borderRadius = `${border.topL}px ${border.topR}px ${border.bottomR}px ${border.bottomL}px`;
+    console.log('ok');
+  }, [])
+
   return (
 
 
@@ -12,19 +23,43 @@ function BorderRadius() {
       <div className='sub-container' >
 
         <div id='BorderTopLeft' >
-          <input type="text" id='BorderTopLeft' value="0"></input>
+          <input type="text" id='BorderTopLeft'
+            value={border.topL}
+            onChange={(e) => setBorde({ ...border, topL: e.target.value })
+
+            }>
+
+
+          </input>
         </div>
 
         <div id='BorderTopRight'>
-          <input type="text" id='BorderTopRight' value="0"></input>
+          <input type="text" id='BorderTopRight'
+            value={border.topR}
+            onChange={(e) => setBorde({ ...border, topR: e.target.value })
+            }
+          >
+          </input>
         </div>
 
         <div id='BorderBottomLeft'>
-          <input type="text" id='BorderBottomLeft' value="0"></input>
+          <input type="text" id='BorderBottomLeft'
+            value={border.bottomL}
+            onChange={(e) => setBorde({ ...border, bottomL: e.target.value })
+            }
+          >
+
+          </input>
         </div>
 
         <div id='BorderBottomRight'>
-          <input type="text" id='BorderBottomLeft' value="0"></input>
+          <input type="text" id='BorderBottomLeft'
+            value={border.bottomR}
+            onChange={(e) => setBorde({ ...border, bottomR: e.target.value })
+            }
+          >
+
+          </input>
         </div>
 
         <div className='txt-area'>
@@ -34,13 +69,13 @@ function BorderRadius() {
 
         <div className='sub-area'>
           <input type='checkbox' id='webkit'></input>
-          <label for='webkit'>Webkit</label>
+          <label htmlFor='webkit'>Webkit</label>
 
           <input type='checkbox' id='gecko'></input>
-          <label for='gecko'>Gecko</label>
+          <label htmlFor='gecko'>Gecko</label>
 
           <input type='checkbox' id='css'></input>
-          <label for='css'>Css</label>
+          <label htmlFor='css'>Css</label>
         </div>
 
 
